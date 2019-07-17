e9c94008e7c26c1bd9a065790f68072238c42030
ozgunasli <ozgunn.asli@gmail.com>
2019-07-17 14:53:15 +0300

    Edited

--- food/urls.py
+++ food/urls.py
@@ -5,8 +5,9 @@ from .views import *
 urlpatterns = [
     path('index/', index_view,name='index'),
     path('create/', create_view,name='create'),
-    #path('ingredient/',ingredient_view,name='ingredient'),
-    re_path(r'^(?P<id>\d+)/$',ingredient_view,name='ingredient'),
+    path('ingredient/<int:id>/',ingredient_view,name='ingredient'),
+
+    #re_path(r'^(?P<id>\d+)/$',ingredient_view,name='ingredient'),
     re_path(r'^(?P<slug>[\w-]+)/$',detail_view,name='detail'),
     re_path(r'^(?P<slug>[\w-]+)/update/$',update_view,name='update'),
 ]
